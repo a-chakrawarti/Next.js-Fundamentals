cc8e3ab131ef5e2c671d1b67ba842145b553e98e
CURVX <chakrawarti.anupam@gmail.com>
2021-05-12 22:51:24 +0530

    useRouter to get access to URL segments & render them

--- pages/mobiles/[brand]/[model].tsx
+++ pages/mobiles/[brand]/[model].tsx
@@ -1,5 +1,29 @@
+import { useRouter } from "next/router";
+
 const Model = () => {
-  return <div>C1-01</div>;
+  // In production, it will not work for the first time
+  // The reason being, without prefetching condition, query object is empty
+  // Prefetching: getServerSideProps, getStaticProps
+  const router = useRouter();
+  const {
+    query: { brand, model },
+  } = router;
+  //   console.log(brand, model);
+
+  const goBackHome = () => {
+    // push home to stack of history
+    // router.push("/");
+    // would replace that particular page from stack history altogether
+    // Try to use link component as much as possible, good for SEO, accessibility
+    router.replace("/");
+  };
+  return (
+    <div>
+      <h3>Brand: {brand}</h3>
+      <p>Model: {model}</p>
+      <button onClick={goBackHome}>Home</button>
+    </div>
+  );
 };
 
 export default Model;
