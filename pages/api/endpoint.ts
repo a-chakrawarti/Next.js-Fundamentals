b96ad487b6c3663b32eeeab60ae94da222ce4ac8
CURVX <chakrawarti.anupam@gmail.com>
2021-05-12 18:59:30 +0530

    res & req Objects

--- pages/api/endpoint.ts
+++ pages/api/endpoint.ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
+  // Request: ?q=<query>
+  console.log("Request Query: ", req.query);
+
+  // Response
+  res.statusCode = 200;
+  res.setHeader("Set-Cookie", "VSCode");
   res.json({ status: "ok", num: Math.floor(Math.random() * 10) });
 }
 
